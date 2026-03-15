1ba4a6316d9b3a8aee8207cfc28cd21082c3b19c
Anthony Stiles <stilant456@gmail.com>
2026-03-15 00:03:56 +0000

    add migration for entity property settings configuration

---
+++ Invoice-Generator.Infrastructure/Data/Migrations/20260315000026_AddPropertySettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invoice_Generator.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertySettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
